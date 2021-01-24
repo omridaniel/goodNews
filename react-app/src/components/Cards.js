9f168c111aeb927969b051e5aa04a8ea2dc38b0e
Omri Daniel <omridaniel13@gmail.com>
2021-01-24 03:09:48 -0500

    changed site title

--- react-app/src/components/Cards.js
+++ react-app/src/components/Cards.js
@@ -1,25 +1,26 @@
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button'
 
-const  Cards = ({key, title, excerpt, link, image}) => {
+const Cards = ({ key, title, excerpt, link, image }) => {
   return (
     <div class="card">
-                <div className="cardStyle2">
-                  <div className="card-body">
-                    <div className="row">
-                      <div className="col" style={{maxWidth: "30%"}}><img src={image} style={{maxWidth: "100%",maxHeight: "95%",borderRadius: "20px",}}/></div>
-                      <div className="col" style={{maxWidth: "100%"}}>
-                        <h4 className="text-right">{title}</h4>
-                        <p style={{maxWidth: "100%", textAlign: "right"}}>{excerpt}</p>
-                      </div>
-                    </div>
-                    <div className="row">
-                    <div className="col text-left" style={{marginTop: "15px"}}>
-                        <a href={link} className="btn btn-light" target="_blank">Visit Source</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+      <div className="cardStyle2">
+        <div className="card-body">
+          <div className="row">
+            <div className="col" style={{ maxWidth: "30%" }}><img src={image} style={{ maxWidth: "100%", maxHeight: "95%", borderRadius: "20px" }} /></div>
+            <div className="col" style={{ maxWidth: "100%" }}>
+              <h4 className="text-right">{title}</h4>
+              <p style={{ maxWidth: "100%", textAlign: "right" }}>{excerpt}</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col text-left" style={{ marginTop: "15px" }}>
+              <a href={link} target="_blank"><Button variant="link">Visit Source</Button></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
